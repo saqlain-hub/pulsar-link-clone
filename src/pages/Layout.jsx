74f25fdf67aa036ae4a0873c85aea185d016b8b2
Saqlain Javed <saqlainsj12@gmail.com>
2023-12-31 00:39:00 +0500

    almost done with homepage ui

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -8,14 +8,14 @@ const Layout = () => {
   return (
     <div className="container mx-auto h-full px-4">
       <div className="grid-container py-2">
-        <aside className="overflow-y-scroll hidden lg:h-[450px] xl:h-[570px]  2xl:h-screen 2xl:overflow-hidden px-2 lg:block sticky top-14 ">
+        <aside className="overflow-y-scroll hidden lg:h-[450px] xl:h-[570px]  2xl:h-screen 2xl:overflow-hidden px-2 lg:block sticky top-16 ">
           <ProfileComp />
           <Followers />
         </aside>
         <main className="md:px-0 ">
           <Outlet /> {/**Homepage */}
         </main>
-        <aside className="hidden max-h-[530px] overflow-hidden xl:block sticky top-14 bg-gray-200 rounded-md">
+        <aside className="hidden max-h-[530px] overflow-hidden xl:block sticky top-16 bg-gray-200 rounded-md">
           <Trends />
         </aside>
       </div>
