1d97963cf0e72c7d6cbab0d7a586a186a9504462
Saqlain Javed <saqlainsj12@gmail.com>
2023-12-31 14:11:41 +0500

    implementing trends

--- src/components/Trends.jsx
+++ src/components/Trends.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const Trends = () => {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col bg-white">
       <h3 className="text-center text-2xl py-2 bg-gray-800 text-white">
         Trending
       </h3>
-      <div className="h-[2px] self-center w-[100%] bg-gray-500"></div>
+      <div className="h-[2px] self-center w-[100%] bg-gray-600"></div>
       <div className="px-2 flex flex-col gap-2 mt-4 overflow-auto h-[530px]">
         <div className="border-2 bg-gray-600 text-white border-white p-2">
           <h4>#FATF</h4>
