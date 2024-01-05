b916ac78c53caacf5acb7e5444a13253ab3c576b
Saqlain Javed <saqlainsj12@gmail.com>
2024-01-06 01:43:15 +0500

    register ui styles

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -40,63 +40,66 @@ const Register = () => {
     navigate("/");
   };
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="firstName">First Name</label>
-        <input
-          type="text"
-          placeholder="John"
-          value={firstName}
-          onChange={(e) => setFirstName(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="lastName">Last Name</label>
-        <input
-          type="text"
-          placeholder="Doe"
-          value={lastName}
-          onChange={(e) => setLastName(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="occupation">Occupation</label>
-        <input
-          type="text"
-          placeholder="Occupation"
-          value={occupation}
-          onChange={(e) => setOccupation(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="image">Image Path</label>
-        <input
-          type="text"
-          placeholder="imagePath"
-          value={image}
-          onChange={(e) => setImage(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          placeholder="Something@gmail.com"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
-      <button>Register</button>
-    </form>
+    <div className="flex justify-center items-center bg-gray-400 h-screen">
+      <form className="w-[800px] bg-gray-300" onSubmit={handleSubmit}>
+        <h3>Register User</h3>
+        <div>
+          <label htmlFor="firstName">First Name</label>
+          <input
+            type="text"
+            placeholder="John"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="lastName">Last Name</label>
+          <input
+            type="text"
+            placeholder="Doe"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="occupation">Occupation</label>
+          <input
+            type="text"
+            placeholder="Occupation"
+            value={occupation}
+            onChange={(e) => setOccupation(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="image">Image Path</label>
+          <input
+            type="text"
+            placeholder="imagePath"
+            value={image}
+            onChange={(e) => setImage(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            placeholder="Something@gmail.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button>Register</button>
+      </form>
+    </div>
   );
 };
 
