7b32eb37be654d73abbb3b64e6c1c478d3cf5dad
Saqlain Javed <saqlainsj12@gmail.com>
2024-01-07 20:35:45 +0500

    register ui done and model done api done

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -40,64 +40,79 @@ const Register = () => {
     navigate("/");
   };
   return (
-    <div className="flex justify-center items-center bg-gray-400 h-screen">
-      <form className="w-[800px] bg-gray-300" onSubmit={handleSubmit}>
-        <h3>Register User</h3>
-        <div>
+    <div className="flex justify-center items-center bg-slate-800 h-screen">
+      <form
+        className=" w-[800px] bg-gray-500 rounded-md py-4 px-16"
+        onSubmit={handleSubmit}
+      >
+        <h3 className="text-white text-2xl text-center mb-4 border-b-2 p-2 border-yellow-400">
+          Register User
+        </h3>
+        <div className="flex flex-col items-center">
           <label htmlFor="firstName">First Name</label>
           <input
+            className="py-1 px-2 rounded-md"
             type="text"
             placeholder="John"
             value={firstName}
             onChange={(e) => setFirstName(e.target.value)}
           />
         </div>
-        <div>
+        <div className="flex flex-col items-center">
           <label htmlFor="lastName">Last Name</label>
           <input
+            className="py-1 px-2 rounded-md"
             type="text"
             placeholder="Doe"
             value={lastName}
             onChange={(e) => setLastName(e.target.value)}
           />
         </div>
-        <div>
+        <div className="flex flex-col items-center">
           <label htmlFor="occupation">Occupation</label>
           <input
+            className="py-1 px-2 rounded-md"
             type="text"
             placeholder="Occupation"
             value={occupation}
             onChange={(e) => setOccupation(e.target.value)}
           />
         </div>
-        <div>
+        <div className="flex flex-col items-center">
           <label htmlFor="image">Image Path</label>
           <input
+            className="py-1 px-2 rounded-md"
             type="text"
             placeholder="imagePath"
             value={image}
             onChange={(e) => setImage(e.target.value)}
           />
         </div>
-        <div>
+        <div className="flex flex-col items-center">
           <label htmlFor="email">Email</label>
           <input
+            className="py-1 px-2 rounded-md"
             type="email"
             placeholder="Something@gmail.com"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
         </div>
-        <div>
+        <div className="flex flex-col items-center">
           <label htmlFor="password">Password</label>
           <input
+            className="py-1 px-2 rounded-md"
             type="password"
             placeholder="Password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
         </div>
-        <button>Register</button>
+        <div className="flex flex-col items-center my-6">
+          <button className="btn rounded-md hover:bg-yellow-400">
+            Register
+          </button>
+        </div>
       </form>
     </div>
   );
